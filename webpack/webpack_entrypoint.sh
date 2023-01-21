81080f9a651564bb22c19664e26f9827dd79f502
Eric Kansa <kansaeric@gmail.com>
2023-01-20 16:28:45 -0800

    Another attempt to build_production

--- webpack/webpack_entrypoint.sh
+++ webpack/webpack_entrypoint.sh
@@ -6,9 +6,10 @@ run_webpack() {
 	echo "----- *** RUNNING WEBPACK DEVELOPMENT SERVER *** -----"
 	echo ""
 	if [[ ${BUILD_PRODUCTION} == 'True' ]]; then
-		echo "Running Webpack, hopefully the yarn build_production thing will work!"
-		cd ${APP_COMP_FOLDER}
-		exec sh -c "yarn install && wait-for-it arches:8000 -t 1200 && yarn build_production"
+		echo "Running Webpack, hopefully the build_production thing will work!"
+		cd ${APP_FOLDER}
+		exec sh -c "yarn install && wait-for-it arches:8000 -t 1200 && python3 manage.py build_production"
+		# exec sh -c "yarn install && wait-for-it arches:8000 -t 1200 && yarn build_production"
 		# echo "Do the build_production thing"
 		# cd ${APP_FOLDER}
 		# python3 manage.py build_production
