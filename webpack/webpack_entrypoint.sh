9c9d760bad5a963bb26c0e8aa79d59fba878286c
Eric Kansa <kansaeric@gmail.com>
2023-03-14 13:13:36 -0700

    Added a wait for it like delay for webpack

--- webpack/webpack_entrypoint.sh
+++ webpack/webpack_entrypoint.sh
@@ -3,4 +3,8 @@
 cd /action_dir
 echo "Start Arches Webpack via Docker"
 echo "docker compose run oc run_worker"
+until nc -z arches 8000; do
+	echo "Waiting for Arches to start..."
+  	sleep 5s & wait ${!}
+done
 docker compose run arches run_webpack
