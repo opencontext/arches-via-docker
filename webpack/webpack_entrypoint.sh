a5ae041a8df3a75c25d5491557b12455c0612996
Eric Kansa <kansaeric@gmail.com>
2023-08-22 08:42:18 -0700

    Update for dev/6.2.x HER

--- webpack/webpack_entrypoint.sh
+++ webpack/webpack_entrypoint.sh
@@ -4,7 +4,7 @@ cd /workdir
 echo "Start Arches Webpack via Docker"
 echo "docker compose run arches run_webpack"
 until nc -z arches 8000; do
-	echo "Waiting for the arches server application to start..."
+	echo "Waiting for the arches server application to start up..."
   	sleep 5s & wait ${!}
 done
 exec docker compose exec --no-TTY arches ./entrypoint.sh run_setup_webpack
