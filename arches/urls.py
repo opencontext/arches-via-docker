82498b3bea2ce805d20497e7ab64c8b604a1c2c2
Eric Kansa <kansaeric@gmail.com>
2023-10-19 17:26:03 -0700

    Fixed Django compatibility issue

--- arches/urls.py
+++ arches/urls.py
@@ -1,16 +1,10 @@
-from django.conf.urls import include
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.conf.urls.i18n import i18n_patterns
+
 
 urlpatterns = [
-    url(r'^', include('arches.urls')),
-    url(r"^", include("arches_for_science.urls")),
+    re_path(r'^', include('arches.urls')),
+    re_path(r"^", include("arches_for_science.urls")),
     path("reports/", include("arches_templating.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# The language URL patterns with i18n_patterns seem a little buggy,
-# so they are disabled with the "if False..." below. Remove
-# the False if you want to try to use this feature.
-if False and settings.SHOW_LANGUAGE_SWITCH is True:
-    urlpatterns = i18n_patterns(*urlpatterns)
