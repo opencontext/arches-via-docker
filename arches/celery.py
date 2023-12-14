cd819e274baff7b71f22d0d6c6cae64413b10993
Eric Kansa <kansaeric@gmail.com>
2023-12-14 08:53:01 -0800

    Updated celery setting

--- arches/celery.py
+++ arches/celery.py
@@ -5,6 +5,6 @@ from celery import Celery
 ARCHES_PROJECT = os.getenv('ARCHES_PROJECT')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{ARCHES_PROJECT}.settings')
-app = Celery('ARCHES_PROJECT')
+app = Celery(ARCHES_PROJECT)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
