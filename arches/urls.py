b20aace139a025735eb79dde1075dc866f3cb4e8
Eric Kansa <kansaeric@gmail.com>
2024-10-24 11:29:19 -0700

    Update URLs, turn off language switch

--- arches/urls.py
+++ arches/urls.py
@@ -11,12 +11,16 @@ urlpatterns = [
 urlpatterns.append(path('', include('arches.urls')))
 
 # Adds URL pattern to serve media files during development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    path("", include("arches.urls")),
+    path("", include("arches_for_science.urls")),
+    path("reports/", include("arches_templating.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Only handle i18n routing in active project. This will still handle the routes provided by Arches core and Arches applications,
 # but handling i18n routes in multiple places causes application errors.
 if settings.ROOT_URLCONF == __name__:
-    if settings.SHOW_LANGUAGE_SWITCH is True:
+    if False and settings.SHOW_LANGUAGE_SWITCH is True:
         urlpatterns = i18n_patterns(*urlpatterns)
 
     urlpatterns.append(path("i18n/", include("django.conf.urls.i18n")))
