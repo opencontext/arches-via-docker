943b12f6d9e6acfae9c837f6906335f9996c8ab7
Eric Kansa <kansaeric@gmail.com>
2024-11-13 14:49:23 -0800

    Removed webpack container for simplicity

--- webpack/webpack_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /workdir
-echo "Start Arches Webpack via Docker"
-echo "docker compose run arches run_webpack"
-# exec docker compose exec --no-TTY arches ./entrypoint.sh run_setup_webpack
