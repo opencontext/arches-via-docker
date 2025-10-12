ddc535f959c398b81b45e43f1c73320b7b530e56
Eric Kansa <kansaeric@gmail.com>
2025-10-11 17:23:49 -0700

    fixed path location

--- arches/entrypoint.sh
+++ arches/entrypoint.sh
@@ -3,7 +3,7 @@
 # APP and YARN folder locations
 APP_FOLDER=${APP_ROOT}
 APP_COMP_FOLDER=${APP_COMP_FOLDER}
-GUNICORN_CONFIG_PATH=${APP_COMP_FOLDER}/media/node_modules/arches/docker/gunicorn_config.py
+GUNICORN_CONFIG_PATH=${APP_COMP_FOLDER}/gunicorn_config.py
 STATIC_ROOT=/static_root
 STATIC_JS=${STATIC_ROOT}/js
 
