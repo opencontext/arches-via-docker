cf0add6b66981b3612a96082a44346771fe368aa
Eric Kansa <kansaeric@gmail.com>
2026-02-09 20:54:34 -0800

    Change cache backend to Redis for default cache

--- arches/settings.py
+++ arches/settings.py
@@ -315,12 +315,13 @@ SESSION_COOKIE_NAME = f"{APP_NAME}_{APP_VERSION}"
 
 # For more info on configuring your cache: https://docs.djangoproject.com/en/2.2/topics/cache/
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://arches_redis:6379/1",
     },
-    'user_permission': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'user_permission_cache',
+    "user_permission": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "user_permission_cache",
     },
 }
 
