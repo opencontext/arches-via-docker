093c663a105862fd3b81035182c0b21da1da82f9
Eric Kansa <kansaeric@gmail.com>
2026-06-16 14:18:33 -0700

    Updated names of containers

--- webpack/webpack_entrypoint.sh
+++ webpack/webpack_entrypoint.sh
@@ -7,4 +7,4 @@ until nc -z arches_her 8000; do
 	echo "Waiting for the arches server application to start..."
   	sleep 5s & wait ${!}
 done
-exec docker compose exec --no-TTY arches_her ./entrypoint.sh run_setup_webpack
+exec docker compose exec --no-TTY arches_megaj ./entrypoint.sh run_setup_webpack
